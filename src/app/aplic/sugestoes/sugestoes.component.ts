e9b2e5a79a9779b4c7630abae9b4816bc711b981
isconaifa <iscobasdostdbv@gmail.com>
2025-07-11 13:56:01 -0400

    commit do dia 11/07/2025

--- src/app/aplic/sugestoes/sugestoes.component.ts
+++ src/app/aplic/sugestoes/sugestoes.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-sugestoes',
   imports: [
+    PanelModule,
+    FormsModule, 
+    CommonModule
   ],
   templateUrl: './sugestoes.component.html',
   styleUrl: './sugestoes.component.css'
 })
 export default class SugestoesComponent {
-
+  
 }
